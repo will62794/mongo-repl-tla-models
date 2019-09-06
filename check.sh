6c52e5f92e62e9d6e40b1a06836380d59a353817
William Schultz <william.schultz@mongodb.com>
2019-09-06 10:42:28 -0400

    Update the checker scripts

--- check.sh
+++ check.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 #
-# Check a model in this directory.
+# Check a model in this directory and save output.
 #
+
 
 workers=$1
 model=$2
 spec=$3
+resultdir="results"
+mkdir -p $resultdir
 echo "Running model: $model, with spec: $spec, workers=$workers"
-java tlc2.TLC -workers $workers -config $model $spec 
+java tlc2.TLC -workers $workers -config $model $spec > "$resultdir/$model.out"
 rm -rf states
